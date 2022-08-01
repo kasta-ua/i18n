dca6477f365f9e4aa12d664dadc4103c7840107e
Alexander Solovyov <alexander@solovyov.net>
2022-08-01 10:51:21 +0300

    1.0.14

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject ua.kasta/i18n "1.0.13"
+(defproject ua.kasta/i18n "1.0.14"
   :description "Internationalization lib"
   :url "https://github.com/kasta-ua/i18n"
   :license {:name "EPL-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3" :scope "provided"]
-                 [org.clojure/clojurescript "1.11.4" :scope "provided"]]
-  :repl-options {:init-ns kasta.i18n}
+  :dependencies [[org.clojure/clojure "1.11.1" :scope "provided"]
+                 [org.clojure/clojurescript "1.11.60" :scope "provided"]]
+  :profiles {:dev {:dependencies [[criterium "0.4.6"]]
+                   :repl-options {:init-ns kasta.i18n}}}
   :source-paths ["src"])
